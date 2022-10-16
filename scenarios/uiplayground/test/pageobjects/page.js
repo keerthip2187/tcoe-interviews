4ea38d2db29aa3d9f961eac59e91562516881fe6
Rivera <rivera.testlio@gmail.com>
2022-10-16 09:03:18 -0400

    Updating base file

--- scenarios/uiplayground/test/pageobjects/page.js
+++ scenarios/uiplayground/test/pageobjects/page.js
@@ -7,7 +7,12 @@ module.exports = class Page {
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    open (path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
+    open (path='') {
+        return browser.url(`http://uitestingplayground.com/${path}`)
     }
+
+    // waitUntilLoaded(cssSelector){
+    //     let result =  browser.waitUntil($(), 15000);
+    //     return result;
+    // }
 }
